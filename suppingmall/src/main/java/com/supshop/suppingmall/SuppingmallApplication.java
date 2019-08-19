3c7b14896da856c1abb6186fe9ec40d32eb9f0e5
김문섭 <kmsup2@gmail.com>
2019-08-19 23:25:50 +0900

    start project

---
+++ suppingmall/src/main/java/com/supshop/suppingmall/SuppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.supshop.suppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppingmallApplication.class, args);
+    }
+
+}
